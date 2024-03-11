886260d2df803151fa4df946483a1a9b8692dd46
Sanna Wiklund <sanna.wiklund98@gmail.com>
2024-03-11 14:49:53 +0100

    Added water-plant test + edited add/remove plant tests

--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -6,19 +6,16 @@ test('Add a new plant to "MyPlants"', async ({ page }) => {
 
     let searchInput = page.locator('#plant-display-text');
     await searchInput.fill('aloe');
-    await seachInput.press('Enter');
+    await searchInput.press('Enter');
     await expect(page.getByText('Aloe Vera')).toBeVisible();
 
-    // Kolla så att sökresultatet är synligt
-    let aloeVeraElement = await page.waitForSelector('.plant-list)'); //ev li:has-text("Aloe Vera")'
+    let aloeVeraElement = await page.waitForSelector('li:has-text("Aloe Vera")'); 
     expect(aloeVeraElement).toBeTruthy();
 
-    await aloeVeraElement.click('button:has-text("Add to my plants")');
+    await page.click('button:has-text("Add to my plants")');
 
-    // Verifiera att plantan har lagts till i "MyPlants"
-
-    let addedPlant = await page.waitForSelector('.my-plants'); // ev 'li:has-text("Aloe Vera")'
-    await expect(addedPlant).toBeVisible();
+    let addedPlant = await page.waitForSelector('li:has-text("Aloe Vera")');
+    await expect(addedPlant).toBeTruthy();
 
 });
 
@@ -32,19 +29,23 @@ test('Add and remove plant from My Plants list', async ({ page }) => {
     await page.click('button:has-text("Search")');
     await page.click('button:has-text("Add to my plants")');
 
-    await page.waitForTimeout(1000);
-
     await page.click('button:has-text("Remove Plant")');
-
-    await page.waitForTimeout(1000);
 
     const isPlantRemoved = !(await page.isVisible('h3:has-text("Monstera")'));
     expect(isPlantRemoved).toBe(true);
 });
 
 //Water
-test('Water a plant in "MyPlants"', async ({ page }) => {
+test('Add and water a plant in "MyPlants"', async ({ page }) => {
     await page.goto('http://127.0.0.1:5500/index.html');
 
-    expect(newNote).toBeTruthy();
+    await page.fill('input[id=plant-display-text]', 'Ficus');
+    await page.click('button:has-text("Search")');
+    await page.click('button:has-text("Add to my plants")');
+
+    await page.click('button:has-text("Water me please! 💧")');
+
+    const wateredPlant = await page.isVisible('button:has-text("All good! 😊")');
+
+    expect(wateredPlant).toBe(true);
 });
