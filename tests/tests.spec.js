223467a82fc5bf206e4ec6fd5164304be5fcb610
Sanna Wiklund <sanna.wiklund98@gmail.com>
2024-03-11 14:22:45 +0100

    Remove plant - test

--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -8,10 +8,23 @@ test('Add a new plant to "MyPlants"', async ({ page }) => {
 });
 
 //Remove
-test('Remove a plant from "MyPlants"', async ({ page }) => {
+test('Add and remove plant from My Plants list', async ({ page }) => {
     await page.goto('http://127.0.0.1:5500/index.html');
 
-    expect(newNote).toBeTruthy();
+    await page.waitForLoadState('domcontentloaded');
+
+    await page.fill('input[id=plant-display-text]', 'Monstera');
+    await page.click('button:has-text("Search")');
+    await page.click('button:has-text("Add to my plants")');
+
+    await page.waitForTimeout(1000);
+
+    await page.click('button:has-text("Remove Plant")');
+
+    await page.waitForTimeout(1000);
+
+    const isPlantRemoved = !(await page.isVisible('h3:has-text("Monstera")'));
+    expect(isPlantRemoved).toBe(true);
 });
 
 //Water
